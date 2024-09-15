8886af7c0edee7f7d6e6e167cf5b4e234017be7e
Michael J <30031129@cc.peralta.edu>
2024-09-15 16:41:00 -0700

    added loop to ask three names and print the first one

--- Names.cpp
+++ Names.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
 using namespace std;
-
 int main(){
+    string names[3];
+    string currentName;
+    cout << "Please write down three names" << "\n";
+    for(int i = 0; i < 3; i++){
+        cin >> currentName;
+        names[i] = currentName;
+    }
+    cout << "First name enterd: " << names[0];
+    
+    
 
 }
